d0608888e77479433854673852e22d7988c8cf5c
Gaspar-Farja <g.farja@itsv.edu.ar>
2026-08-10 11:48:36 +0000

    chore: acerca_de_mi.html

--- config/urls.py
+++ config/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tiendalibre.views import inicio
+
 from django.conf import settings
 from django.conf.urls.static import static
 
+from tiendalibre.views import inicio, acerca_de_mi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', inicio.as_view())
-]
+    path('inicio/', inicio.as_view()),
+    path('acerca_de_mi/', acerca_de_mi.as_view())
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tiendalibre/views.py
+++ tiendalibre/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
-from django.views.generic import TemplateView
 
 class inicio(TemplateView):
     template_name = 'inicio.html'
+
+
+class acerca_de_mi(TemplateView):
+    template_name = 'acerca_de_mi.html'
